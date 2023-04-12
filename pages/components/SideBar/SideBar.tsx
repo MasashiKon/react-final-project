46d49c2ded40a6879c83c93101553926d81937a7
MasahiKon <konnonorth@gmail.com>
2023-04-11 22:34:54 -0700

    first commit

---
+++ pages/components/SideBar/SideBar.tsx
@@ -0,0 +1,11 @@
+import { StyledContainer } from "./Sidebar.style";
+
+import React from 'react'
+
+function SideBar() {
+  return (
+    <StyledContainer>SideBar</StyledContainer>
+  )
+}
+
+export default SideBar
---
+++ pages/components/SideBar/Sidebar.style.ts
@@ -0,0 +1,24 @@
+import styled from "styled-components";
+import { motion } from "framer-motion";
+
+export const StyledContainer = styled(motion.div)`
+    background-color: black;
+    height: 100vh;
+    width: 10vw;
+
+    @media (min-width: 576px) {
+
+    }
+
+    @media (min-width: 768px) {
+        flex-direction: column;
+    }
+
+    @media (min-width: 992px) {
+        width: 20vw;
+    }
+
+    @media (min-width: 1200px) {
+        
+    }
+`;
