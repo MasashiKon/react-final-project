4a3e927aa80194ecdd544ee175f6c4cad50020d4
MasahiKon <konnonorth@gmail.com>
2023-04-22 04:18:42 -0700

    proceding next-auth

--- components/View/View.tsx
+++ components/View/View.tsx
@@ -5,10 +5,24 @@ import Status from '../Status/Status'
 import SideBar from '../SideBar/SideBar'
 import Main from '../Main/Main'
 
+import { useSession, signIn, signOut } from "next-auth/react"
+
 function View({children}) {  
+  const { data: session, status } = useSession()
   return (
     <StyledViewContainer>
         <SideBar />
+          {session ? (
+      <>
+        Signed in as {session.user.email} <br />
+        <button onClick={() => signOut()}>Sign out</button>
+      </>
+    ) : (
+      <>
+        Not signed in <br />
+        <button onClick={() => signIn()}>Sign in</button>
+      </>
+    )}
           {children}
         <Status />
     </StyledViewContainer>
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,8 @@
 import type { AppProps } from 'next/app'
 import { createGlobalStyle, ThemeProvider } from 'styled-components'
 
+import { SessionProvider } from "next-auth/react"
+
 import View from '../components/View/View'
 
 const GlobalStyle = createGlobalStyle`
@@ -23,15 +25,17 @@ const theme: ThemeInterface = {
   },
 }
 
-export default function App({ Component, pageProps }: AppProps) {
+export default function App({ Component, pageProps: {session, ...pageProps} }: AppProps) {
   return (
     <>
-      <GlobalStyle />
-      <ThemeProvider theme={theme}>
-        <View>
-          <Component {...pageProps} />
-        </View>
-      </ThemeProvider>
+      <SessionProvider session={session}>
+        <GlobalStyle />
+        <ThemeProvider theme={theme}>
+          <View>
+            <Component {...pageProps} />
+          </View>
+        </ThemeProvider>
+      </SessionProvider>
     </>
   )
 }
