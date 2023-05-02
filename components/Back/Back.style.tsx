22acf219ad278d3f2487829f6633773c0f030cc7
MasahiKon <konnonorth@gmail.com>
2023-05-01 20:23:31 -0700

    rmv dupe file

--- components/Back/Back.style.tsx
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-
-export const StyledMainBackground = styled.div`
-    height: 100vh;
-    width: 60vw;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-`
