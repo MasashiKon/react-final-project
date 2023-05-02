1aba7362a6ab9cd38629fef859f2e45e9a129e87
MasahiKon <konnonorth@gmail.com>
2023-05-01 20:10:10 -0700

    add google auth

---
+++ components/Back/Back.style.tsx
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const StyledMainBackground = styled.div`
+    height: 100vh;
+    width: 60vw;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`
